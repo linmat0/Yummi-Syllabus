6733039499d0a2137ea2696f2f5a019665bd8687
Matthew Lin <Lin.M@etoncollege.org.uk>
2024-01-23 13:00:53 +0000

    Initial Commit

---
+++ Yummi Syllabus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yummi Syllabus
+//
+//  Created by Lin, Matthew (CDNP) on 23/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yummi Syllabus/Yummi_SyllabusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yummi_SyllabusApp.swift
+//  Yummi Syllabus
+//
+//  Created by Lin, Matthew (CDNP) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Yummi_SyllabusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
